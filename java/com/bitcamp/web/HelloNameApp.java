b540211eb94091c48d02df4fd362596df56f510c
kimseunghyun <suji1541@gmail.com>
2022-01-19 13:02:40 +0900

    2022-01-19 13:02 id pw name 앱

---
+++ java/com/bitcamp/web/HelloNameApp.java
@@ -0,0 +1,25 @@
+package com.bitcamp.web;
+
+import java.util.Scanner;
+
+public class HelloNameApp{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("id");
+        String id = scanner.next();
+        System.out.println("pw");
+        String pw = scanner.next();
+        System.out.println("name");
+        String name = scanner.next();
+        System.out.println("welcome id "+id+" pw "+pw+" name "+name);
+
+;
+
+
+
+
+    }
+
+
+
+}
